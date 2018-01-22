a6d562fd3701dece631c944034c58fe9ae80638b
Bitholddev <35224623+Bitholddev@users.noreply.github.com>
2018-01-22 19:42:10 +0200

    max connections limit to 16
    max connections limit to 16 due to over-consumption of traffic in some cases.

--- src/net.cpp
+++ src/net.cpp
@@ -36,7 +36,7 @@
 using namespace std;
 using namespace boost;
 
-static const int MAX_OUTBOUND_CONNECTIONS = 32;
+static const int MAX_OUTBOUND_CONNECTIONS = 16;
 
 bool OpenNetworkConnection(const CAddress& addrConnect, CSemaphoreGrant *grantOutbound = NULL, const char *strDest = NULL, bool fOneShot = false);
 
