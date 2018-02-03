887c2f26f2a8460077c7bb98bea1eddb58a69e29
Bitholddev <35224623+Bitholddev@users.noreply.github.com>
2018-02-03 19:01:29 +0200

    Darksend off
    Darksend off

--- src/coincontrol.h
+++ src/coincontrol.h
@@ -21,7 +21,7 @@ public:
         destChange = CNoDestination();
         setSelected.clear();
         useInstantX = false;
-        useDarkSend = true;
+        useDarkSend = false;
     }
     
     bool HasSelected() const
